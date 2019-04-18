55dc0079f3cce4000fd5197ac1328f4992d2b86d
Anfany <36292421+Anfany@users.noreply.github.com>
2019-04-18 15:13:39 +0800

    Update 20_Valid_Parentheses.py

--- 栈/20_Valid_Parentheses.py
+++ 栈/20_Valid_Parentheses.py
@@ -4,22 +4,22 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        if len(s) == 0:
+        if len(s) == 0:  # 空字符串是有效的
             return True
         else:
-            parentheses = []
-            match_dict = {']': '[',  ')': '(', '}': '{'}
+            parentheses = []  # 栈
+            match_dict = {']': '[',  ')': '(', '}': '{'}  # 匹配的字典
             for i in s:
                 if not parentheses:
                     parentheses.append(i)
                 else:
-                    if i not in match_dict:
-                        parentheses.append(i)
+                    if i not in match_dict:  # 说明是左括号
+                        parentheses.append(i)  # 进栈
                     else:
-                        if match_dict[i] == parentheses[-1]:
-                            parentheses.pop(-1)
+                        if match_dict[i] == parentheses[-1]:  # 可以匹配上
+                            parentheses.pop(-1) # 出栈
                         else:
-                            return False
+                            return False  # 匹配不上，是无效的
             if not parentheses:
                 return True
             else:
